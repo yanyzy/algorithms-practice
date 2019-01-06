1b8fc51edaa35b98e3639464b10a6a90c8913248
yanyzy <1181862765@qq.com>
2019-01-06 22:22:02 +0800

    commit for MSD

--- src/main/java/algorithms/strings/MSD.java
+++ src/main/java/algorithms/strings/MSD.java
@@ -1,10 +1,65 @@
 package algorithms.strings;
 
+import java.util.Arrays;
+
 /**
  * @author zhyee
  * @date 2019/1/6 下午9:30
  */
 public class MSD {
+    private static final int R = 256;
+    private static final int M = 1;
+    private static String[] aux;
 
+    private static int charAt(String s, int d) {
+        if (d < s.length()) {
+            return s.charAt(d);
+        }
+        return -1;
+    }
+
+    public static void sort(String[] a) {
+        int N = a.length;
+        aux = new String[N];
+        sort(a, 0, N - 1, 0);
+
+    }
+
+    public static void sort(String[] a, int lo, int hi, int d) {
+        if (hi < lo + M) {
+            return;
+        }
+        int[] count = new int[R + 2];
+        //计算频率
+        for (int i = lo; i <= hi; i++) {
+            count[charAt(a[i], d) + 2]++;
+        }
+        //将频率转换为索引
+        for (int i = 0; i < R + 1; i++) {
+            count[i + 1] += count[i];
+        }
+        //数据分类
+        for (int i = lo; i <= hi; i++) {
+            aux[count[charAt(a[i], d) + 1]++] = a[i];
+        }
+        //回写
+        for (int i = lo; i <= hi; i++) {
+            a[i] = aux[i - lo];
+        }
+        for (int r = 0; r < R; r++) {
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"she",
+                "sells",
+                "seashells",
+                "she",
+                "by",
+                "the"};
+        MSD.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 
 }
